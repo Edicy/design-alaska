1e42bf8101607f55bcdb071a344bf630686fbc3b
Kaspar Naaber <kaspar@fraktal.ee>
2014-01-02 16:19:02 +0200

    Add recent post gallery area

--- index.php
+++ index.php
@@ -13,14 +13,27 @@
         <header class="recent-header">
           <h1 class="main-title">This family house stands amongst dense pine trees on the outskirts of Alaska</h1>
         </header>
+        <section class="post-thumbnails wrap">
+          <div class="small-thumbs">
+            <img class="post-thumb" src="http://placeimg.com/470/470/any">
+            <img class="post-thumb" src="http://placeimg.com/470/470/any">
+            <img class="post-thumb" src="http://placeimg.com/470/470/any">
+            <img class="post-thumb" src="http://placeimg.com/470/470/any">
+          </div>
+          <div class="large-thumb">
+            <img class="post-thumb" src="http://placeimg.com/470/478/any">
+          </div>
+        </section>
       </article>
+
       <section class="content-slogan">
         <div class="main-title">For fishing with bait, crabs, small fish and cut bait all work very well.</div>
       </section>
+
       <section class="latest-posts wrap">
         <article class="post">
           <header class="post-header">
-            <img class="post-image" src="http://placeimg.com/470/470/any">
+            <img class="post-thumb" src="http://placeimg.com/470/470/any">
             <h2 class="post-title">Active mermaid fishing on lake Abyss</h2>
           </header>
           <div class="post-excerpt">A part of the Glacier Bay National Park and Preserve, its water is regularly.</div>
@@ -28,7 +41,7 @@
 
         <article class="post">
           <header class="post-header">
-            <img class="post-image" src="http://placeimg.com/471/471/any">
+            <img class="post-thumb" src="http://placeimg.com/471/471/any">
             <h2 class="post-title">Fishing from a boat in easy sight</h2>
           </header>
           <div class="post-excerpt">Anglers either use an uptide rod between 9 and 10 feet in length to cast from the boat.</div>
@@ -36,7 +49,7 @@
 
         <article class="post">
           <header class="post-header">
-            <img class="post-image" src="http://placeimg.com/472/472/any">
+            <img class="post-thumb" src="http://placeimg.com/472/472/any">
             <h2 class="post-title">An angler in a float tube plays a pike.</h2>
           </header>
           <div class="post-excerpt">Though they differ in design and are expensive to build and maintain.</div>
@@ -44,7 +57,7 @@
 
         <article class="post">
           <header class="post-header">
-            <img class="post-image" src="http://placeimg.com/473/473/any">
+            <img class="post-thumb" src="http://placeimg.com/473/473/any">
             <h2 class="post-title">Deep sea fishing from a boat in the Gulf </h2>
           </header>
           <div class="post-excerpt">Offshore game fish, like marlin and tuna, can be very large and heavy tackle is needed.</div>
