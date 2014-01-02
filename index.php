5f0c2590bd3179404f546b5ac52c9861ad86a669
Kaspar Naaber <kaspar@fraktal.ee>
2014-01-02 15:24:06 +0200

    Add front page latest posts listing

--- index.php
+++ index.php
@@ -17,6 +17,39 @@
       <section class="content-slogan">
         <div class="main-title">For fishing with bait, crabs, small fish and cut bait all work very well.</div>
       </section>
+      <section class="latest-posts wrap">
+        <article class="post">
+          <header class="post-header">
+            <img class="post-image" src="http://placeimg.com/470/470/any">
+            <h2 class="post-title">Active mermaid fishing on lake Abyss</h2>
+          </header>
+          <div class="post-excerpt">A part of the Glacier Bay National Park and Preserve, its water is regularly.</div>
+        </article>
+
+        <article class="post">
+          <header class="post-header">
+            <img class="post-image" src="http://placeimg.com/471/471/any">
+            <h2 class="post-title">Fishing from a boat in easy sight</h2>
+          </header>
+          <div class="post-excerpt">Anglers either use an uptide rod between 9 and 10 feet in length to cast from the boat.</div>
+        </article>
+
+        <article class="post">
+          <header class="post-header">
+            <img class="post-image" src="http://placeimg.com/472/472/any">
+            <h2 class="post-title">An angler in a float tube plays a pike.</h2>
+          </header>
+          <div class="post-excerpt">Though they differ in design and are expensive to build and maintain.</div>
+        </article>
+
+        <article class="post">
+          <header class="post-header">
+            <img class="post-image" src="http://placeimg.com/473/473/any">
+            <h2 class="post-title">Deep sea fishing from a boat in the Gulf </h2>
+          </header>
+          <div class="post-excerpt">Offshore game fish, like marlin and tuna, can be very large and heavy tackle is needed.</div>
+        </article>
+      </section>
     </main>
 
     <?php require("components/site-footer.php") ?>
