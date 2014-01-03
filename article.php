56e7822d792f17cc5a22d4a49ea5c2d96a458c99
Kaspar Naaber <kaspar@fraktal.ee>
2014-01-03 12:01:26 +0200

    Add article view styles

--- article.php
+++ article.php
@@ -8,26 +8,40 @@
   <section class="site-content cfx">
     <?php require("components/site-header.php") ?>
 
-    <main class="page-content cfx" role="main">
-      <article class="recent-post">
-        <header class="recent-header">
-          <h2 class="content-title">This family house stands amongst dense pine trees on the outskirts of Alaska</h2>
+    <main class="page-content article cfx" role="main">
+      <article class="post">
+        <header class="post-header">
+          <h1 class="post-title">This family house stands amongst dense pine trees on the outskirts of Alaska</h1>
         </header>
-        <section class="post-thumbnails wrap">
-          <div class="small-thumbs">
-            <img class="post-thumb" src="http://placeimg.com/470/470/any">
-            <img class="post-thumb" src="http://placeimg.com/470/470/any">
-            <img class="post-thumb" src="http://placeimg.com/470/470/any">
-            <img class="post-thumb" src="http://placeimg.com/470/470/any">
+        <section class="post-thumbnails">
+          <div class="wrap cfx">
+            <div class="small-thumbs">
+              <img class="post-thumb" src="http://placeimg.com/470/470/any">
+              <img class="post-thumb" src="http://placeimg.com/470/470/any">
+              <img class="post-thumb" src="http://placeimg.com/470/470/any">
+              <img class="post-thumb" src="http://placeimg.com/470/470/any">
+            </div>
+            <div class="large-thumb">
+              <img class="post-thumb" src="http://placeimg.com/470/478/any">
+            </div>
+            <div class="bottom-thumbs">
+              <img class="post-thumb" src="http://placeimg.com/470/470/any">
+              <img class="post-thumb" src="http://placeimg.com/470/470/any">
+              <img class="post-thumb" src="http://placeimg.com/470/470/any">
+              <img class="post-thumb" src="http://placeimg.com/470/470/any">
+            </div>
           </div>
-          <div class="large-thumb">
-            <img class="post-thumb" src="http://placeimg.com/470/478/any">
-          </div>
-          <div class="bottom-thumbs">
-            <img class="post-thumb" src="http://placeimg.com/470/470/any">
-            <img class="post-thumb" src="http://placeimg.com/470/470/any">
-            <img class="post-thumb" src="http://placeimg.com/470/470/any">
-            <img class="post-thumb" src="http://placeimg.com/470/470/any">
+        </section>
+
+        <section class="post-content">
+          <div class="wrap cfx">
+            <div class="post-bind"><h2>The cabin stood quietly among the trees.</div>
+            <div class="post-body">
+              <p>While the lake is exceptional, the hike to it is equally compelling. Visitors will enjoy pristine valleys, abundant aspen, open alpine travel and excellent backcountry camping en route to Abyss Lake: The trail begins on a steady grade through uniform lodgepole and young aspen (1.0 miles : 10,020').</p>
+              <p>It crosses south over Scott Gomer Creek (2.15 miles : 10,162') and levels beside it through dense aspen.</p>
+              <p>There are several established campsites on this stretch with great views up-valley. Look for moose in the willow-shrouded creek corridor. Here the trail becomes steep, faint, and obstructed by several down trees.</p>
+              <p>Though brief, this section is difficult to navigate and reclaim the trail once off course. Pay close attention to your progress and location.</p>
+            </div>
           </div>
         </section>
       </article>
