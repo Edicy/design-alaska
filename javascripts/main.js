fa862c3a7c7f56643c56e44530045e70b2cbc61a
kaspar-naaber <kaspar.naaber@outlook.com>
2014-05-08 09:37:29 +0300

    Add main javascripts

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,117 +1,108 @@
 ;(function($) {
-  // SWITCHES THE SITE LANGUAGE TO THE SELECTED VALUE FROM THE LANGUAGE MENU
-  var handleLanguageSwitch = function() {
-    $('.menu-lang').find('.menu').change(function() {
-      console.log('changed');
-      window.location = $(this).find(':selected').val();
-    });
-  };
+    var toggleMainMenu = function() {
+        $('.js-menu-btn').click(function() {
+            $(this).toggleClass('open');
+            $('.js-menu-main').toggleClass('expanded');
+        });
+    };
 
-  // SHOWS/HIDES THE POPOVER MAIN MENU (VISIBLE ON SMALLES SCREENS)
-  var toggleMainMenu = function() {
-    $('.js-menu-btn').click(function() {
-      $(this).toggleClass('open');
-      $('.js-menu-main').toggleClass('expanded');
-    });
-  };
+    var toggleLangMenu = function() {
+        $('.js-menu-lang-btn').click(function(event) {
+            event.stopPropagation();
+            $('.js-menu-lang-popover').toggleClass('expanded');
+        });
+    };
 
-  // HIDES THE POPOVER MAIN MENU IF CICKED ANYWHERE ELSE THAN THE MENU ITSELF (VISIBLE ON SMALLES SCREENS)
-  var handlePopoverMenuHide = function() {
-    $('html').click(function() {
-      if ($('.js-lang-menu-popover').hasClass('expanded')) {
-        $('.js-lang-menu-popover').removeClass('expanded');
-      }
-    });
-  };
+    var handlePopoverMenuHide = function() {
+        $('html').click(function() {
+            if ($('.js-menu-lang-popover').hasClass('expanded')) {
+                $('.js-menu-lang-popover').removeClass('expanded');
+            }
+        });
+    };
 
-  // HIDES THE FORM FIELD ERROR WHEN THE FIELD IS FOCUSED
-  var handleFormFieldClick = function() {
-    $('.form_field_with_errors').click(function() {
-      $(this).removeClass('form_field_with_errors');
-    });
-  };
+    var handleFormFieldClick = function() {
+        $('.form_field_with_errors').click(function() {
+            $(this).removeClass('form_field_with_errors');
+        });
+    };
 
-  // SCROLLS TO THE COMMENT-FORM IF COMMENT SUBMIT FAILED (TO SHOW THE ERROR MESSAGES TO THE USER)
-  var focusCommentsWithErrors = function() {
-    $(document).ready(function() {
-      if ($('.comment-form').hasClass('form_with_errors') === true) {
-        $('html, body').scrollTop($('.comment-form').offset().top);
-      }
-    });
-  };
+    var focusCommentsWithErrors = function() {
+      $(document).ready(function() {
+        if ($('.comment-form').hasClass('form_with_errors') === true) {
+          $('html, body').scrollTop($('.comment-form').offset().top);
+        }
+      });
+    };
 
-  // TODO: Remove if Edicy is going to wrap table with the container
-  var wrapTables = function() {
-    $('.content-formatted table').wrap('<div class="table-container overthrow"></div>');
-  };
+    var wrapTables = function() {
+        $('.content-formatted table').wrap('<div class="table-container overthrow"></div>');
+    };
 
-  // TODO: Add comment
-  var checkScrollBar = function() {
-    jQuery.fn.hasScrollBar = function(direction) {
-      if (direction == 'vertical') {
-        return this.get(0).scrollHeight > this.innerHeight();
-      } else if (direction == 'horizontal') {
-        return this.get(0).scrollWidth > this.innerWidth();
-      }
-      return false;
-    }
-  };
+    var checkScrollBar = function() {
+        jQuery.fn.hasScrollBar = function(direction) {
+            if (direction == 'vertical') {
+                return this.get(0).scrollHeight > this.innerHeight();
+            } else if (direction == 'horizontal') {
+                return this.get(0).scrollWidth > this.innerWidth();
+            }
+            return false;
+        }
+    };
 
-  // ADDS HORIZONTAL SCROLL TO TABLES THAT DON'T FIT INTO THE CONTENT AREA
-  var handleTableHorizontalScrolling = function() {
-    $.each($('.table-container'), function() {
-      if ($(this).hasScrollBar('horizontal') === true) {
-        $(this).addClass('horizontal-scroll');
-      } else {
-        $(this).removeClass('horizontal-scroll');
-      }
-    });
-  };
+    var handleTableHorizontalScrolling = function() {
+        $.each($('.table-container'), function() {
+            if ($(this).hasScrollBar('horizontal') === true) {
+                $(this).addClass('horizontal-scroll');
+            } else {
+                $(this).removeClass('horizontal-scroll');
+            }
+        });
+    };
 
-  // INITIATES THE TABLE HORISONTAL SCROLL FUNCTION WHEN WINDOW IS RESIZED
-  var handleWindowResize = function() {
-    $(window).resize(function() {
-      handleTableHorizontalScrolling();
-    });
-  };
+    var handleWindowResize = function() {
+        $(window).resize(function() {
+            handleTableHorizontalScrolling();
+        });
+    };
+
+    var getNewArticleURL = function() {
+        newArticleUrl = $('.js-post-add-btn').find('.edy-site-menu-add').attr('href');
+        console.log(newArticleUrl);
+        $('.js-post-add').attr('href', newArticleUrl);
+    };
 
     // Initiations
     var initBlogPage = function() {
-      // ADD BLOG LISTING VIEW SPECIFIC FUNCTIONS HERE
+    };
+
+    var initBlogPageEditmode = function() {
+        getNewArticleURL();
     };
 
     var initArticlePage = function() {
-      // ADD SINGLE POST VIEW SPECIFIC FUNCTIONS HERE
-      handleFormFieldClick();
-      focusCommentsWithErrors();
-    };
-
-    var initCommonPage = function() {
-      // ADD COMMON PAGE SPECIFIC FUNCTIONS HERE
-      handleFormFieldClick();
-      focusCommentsWithErrors();
+        handleFormFieldClick();
+        focusCommentsWithErrors();
     };
 
     var init = function() {
-      // ADD SITE WIDE FUNCTIONS HERE
-      handleLanguageSwitch();
-      toggleMainMenu();
-      handlePopoverMenuHide();
-      handleWindowResize();
-      wrapTables();
-      if ($('.table-container').length > 0) {
-        checkScrollBar();
-        handleTableHorizontalScrolling();
-      }
+        toggleMainMenu();
+        toggleLangMenu();
+        handlePopoverMenuHide();
+        handleWindowResize();
+        wrapTables();
+        if ($('.table-container').length > 0) {
+            checkScrollBar();
+            handleTableHorizontalScrolling();
+        }
     };
 
-    // ENABLES THE USAGE OF THE INITIATIONS OUTSIDE THIS FILE
     window.site = $.extend(window.site || {}, {
-      initBlogPage: initBlogPage,
-      initArticlePage: initArticlePage,
-      initCommonPage: initCommonPage,
-      handleFormFieldClick: handleFormFieldClick
+        initBlogPage: initBlogPage,
+        initBlogPageEditmode: initBlogPageEditmode,
+        initArticlePage: initArticlePage,
+        handleFormFieldClick: handleFormFieldClick
     });
 
     init();
-  })(jQuery);
+})(jQuery);
